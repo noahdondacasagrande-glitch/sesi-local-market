d8e4074f70f60eeb4a0a939440350ac11f88f275
noahdondacasagrande-glitch <noahdondacasagrande@gmail.com>
2026-06-22 12:41:00 -0300

    Add files via upload

---
+++ src/Carrinho/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
+    const listElement = $('#lista')
+    const totalElement = $("#total")
+
+    function exibirCarrinho(){
+        listElement.empty()
+        let totalPreco = 0
+
+        $.each(carrinho, function(index, item){
+            const listItem = $("<li>").text(`${item.desc} - Preço: $${item.preco.toFixed(2)}`)
+
+            const removeButton = $("<button>").text("❌").css("margin-left", "10px").click(function(){
+                removerItem(index)
+            })
+
+            listItem.append(removeButton)
+            listElement.append(listItem)
+
+
+            totalPreco += item.preco
+        })
+        totalElement.text('Total:$$(total.Preco.toFixed(2)}')
+    }
+    function removerItem(index){
+        
+    }
+
+
+})
+
+
